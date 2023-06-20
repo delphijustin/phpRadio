b32e8ffc771ea62caac58277f7e4859745743776
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2023-06-20 17:00:15 -0500

    Create radioConfig.php

---
+++ radioConfig.php
@@ -0,0 +1,8 @@
+<?php 
+/*
+* This file contains settings used by phpRadio
+*/
+$noContentlength = false; //Don't send Content-length. Set to true for Quick 'n' easy web server
+$unusedFirst = true; //If song has already been played and hasn't played all songs try a different song.
+$nsfw = false; //Warn visitors that this radio station has content unsafe for kids, like profanity.
+?>
